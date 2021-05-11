7e9a30e8823c0ce9b37c78eb6216ea70844c37f3
ryutaro1223 <k.ryutaro1223@gmail.com>
2021-05-11 09:57:33 +0000

    非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,23 +6,23 @@ before_action :authenticate_user!
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
-    if @book_comment.save
-  		redirect_to book_path(@book.id)
-    else
-		  render 'books/show' end
-	  
+    @book_comment.save
+    @book_comment = BookComment.new
+    # if @book_comment.save
+  	   #render 'books/show' end
 	end
 
   def destroy
   	@book = Book.find(params[:book_id])
   	book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
+		@book_comment = BookComment.new
+
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-end 
+end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,11 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.create(book_id: params[:book_id])
     @favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
-    redirect_to request.referer
   end
 end
